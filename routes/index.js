09623c309bcce5385d493016e8379db113883f09
p-bradley <p_bradley@fanshaweonline.ca>
2019-12-04 21:18:06 -0500

    finished js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const sql = require('../utils/sql');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Infographic' });
 });
+
+router.get('/svgData/:target', (req, res) => {
+  let query = `SELECT * FROM tbl_facts WHERE id="${req.params.target}"`
+
+  sql.query(query, (err, result) => {
+    if (err){console.log(err);}
+
+    console.log(result);
+
+    res.json(result[0]);
+  })
+})
 
 module.exports = router;
